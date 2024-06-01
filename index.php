0eb29157193cbe7e517c4d4822d60abd62accb82
Stepan Glukharev <pokemonivan32@gmail.com>
2024-06-01 14:14:23 +0300

    Added index.php, style.css files
    index.php - has links to style.css and associated fonts
    style.css - has minimal null style and styles for font

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<title>CRUD</title>
+</head>
+<body class="inter-regular">
+Текст для проверки
+</body>
+</html>
