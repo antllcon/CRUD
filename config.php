a64c0fbb10380a44d5a1af4a1f0a4bf0c849cd1d
Stepan Glukharev <pokemonivan32@gmail.com>
2024-06-01 17:07:31 +0300

    Database connection data file created

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'crud',
+        'username' => 'root',
+        'password' => '1234',
+        'charset' => 'utf8',
+    ],
+];
